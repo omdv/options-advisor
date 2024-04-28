e56fc654ef0e7d1e8d0413057faf6ec8cb1e9b68
omdv <omdv@protonmail.com>
2024-04-28 13:04:07 -0500

    heatmap svg, styles

--- volatility/lookup.py
+++ volatility/lookup.py
@@ -3,10 +3,14 @@ Predicting probability of ITM for a given option
 this file supposed to be used as a standalone API
 """
 import os
+import base64
+from io import BytesIO
+
 import numpy as np
 import pandas as pd
 import yfinance as yf
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def load_itm_dataframe():
     """
@@ -18,6 +22,40 @@ def load_itm_dataframe():
     return pd.read_pickle(path)
 
 
+def generate_heatmap(df):
+    """
+    Generate a heatmap from the given frame
+    save as IO buffer and pass as base64 string
+    """
+
+    original_shape = df.values.shape
+    labels = np.array([f"{v:.3f}" if v > 0.01 else '' for v in df.values.flatten()])
+    labels = labels.reshape(original_shape)
+    df.replace(0, np.NaN, inplace=True)
+
+    plt.figure(figsize=(8,4), dpi=600)
+    sns.heatmap(
+        df,
+        annot=labels,
+        fmt='',
+        annot_kws={'size': 'small', 'alpha': 0.7},
+        linewidths=0.1,
+        linecolor="#4c566a",
+        cbar_kws={'shrink': 0.8},
+        cbar=False,
+        cmap='vlag')
+    plt.tick_params(axis='both', colors='#d8dee9')
+    plt.xticks(color='#d8dee9')
+    plt.yticks(color='#d8dee9')
+    plt.xlabel("delta bin", color='#d8dee9', fontweight='bold')
+    plt.ylabel("expiration", color='#d8dee9', fontweight='bold')
+
+    buf = BytesIO()
+    plt.savefig(buf, format='svg', transparent=True)
+    buf.seek(0)
+    return base64.b64encode(buf.read()).decode('utf-8')
+
+
 def itm_stats(vix_open, otc_open):
     """
     API-ready function
@@ -26,6 +64,10 @@ def itm_stats(vix_open, otc_open):
     df['vix_bins'], vix_bins = pd.qcut(df['vix_open'], q=4, retbins=True)
     df['otc_bins'], otc_bins = pd.qcut(df['open_to_close_pct'], q=4, retbins=True)
 
+    # for sample size
+    group_sizes = df.groupby(['vix_bins','otc_bins']).size()
+
+    # get probabilities
     probs = pd.pivot_table(
         df,
         values='itm',
@@ -41,9 +83,13 @@ def itm_stats(vix_open, otc_open):
     result = probs.loc[(vix_bin, otc_bin, slice(None))].reset_index(level=[0,1], drop=True)
     response['probs'] = result.to_html(
         float_format="{:,.4f}".format,
+        index_names=False,
+        classes='dataframe',
         col_space=10,
     )
-    response['number_of_samples'] = df.shape[0]
+    response['probs_heatmap'] = generate_heatmap(result)
+    response['total_samples'] = df.shape[0]
+    response['group_samples'] = group_sizes.loc[(vix_bin, otc_bin)].values[0]
     response['min_date'] = df['quote_datetime'].min().strftime('%Y-%m-%d')
     response['max_date'] = df['quote_datetime'].max().strftime('%Y-%m-%d')
     return response
@@ -77,6 +123,7 @@ def api_call():
     otc_open, otc_quote_date = get_otc_open()
     stats = itm_stats(vix_open, otc_open)
 
+    result['gen_timestamp'] = pd.Timestamp.now().strftime('%Y-%m-%d %H:%M:%S')
     result['vix_open'] = vix_open
     result['vix_quote_date'] = vix_quote_date.strftime('%Y-%m-%d')
     result['otc_open'] = otc_open
