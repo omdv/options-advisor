95e6a2d0e98ca35326d2439b78ee33e4d45936ef
omdv <omdv@protonmail.com>
2024-05-04 21:21:31 -0500

    removed Hodges Tompkins from ranged estimators

--- volatility/models/api.py
+++ volatility/models/api.py
@@ -3,7 +3,6 @@ api for the module
 """
 
 from volatility.models import garman_klass
-from volatility.models import hodges_tompkins
 from volatility.models import parkinson
 from volatility.models import close_to_close
 from volatility.models import rogers_satchell
@@ -14,7 +13,6 @@ __all__ = [
     "close_to_close",
     "parkinson",
     "garman_klass",
-    "hodges_tompkins",
     "rogers_satchell",
     "yang_zhang",
 ]
